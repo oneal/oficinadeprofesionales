0b5b92068ed6b58584a471f5c9e64e69272bf53f
Amolina <amolina@padelmanager.com>
2024-01-11 00:25:31 +0100

    cambiado modal politia privacidad, quitado texto anuncio destacados home y cambiado el icono de twitter

---
+++ modal_politica_privacidad_resenas.php
@@ -0,0 +1,92 @@
+<div class="modal fade" id="resenaPoliticaPrivacidadModal" tabindex="-1" aria-labelledby="resenaPoliticaPrivacidadModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Política de privacidad</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p><strong>PROTECCIÓN DE DATOS</strong></p>
+                <p><strong>Responsable: </strong></p>
+                <p>F. Carrillo Garrido. En adelante (El titular) Dirección postal: Apartado de correos 2097. 23080
+                    de Jaén. España. E-mail: <a href="general@oficinadeprofesionales.com">general@oficinadeprofesionales.com</a></p>
+                <p><strong>Delegado de Protección de Datos:</strong> Datasur Protección de Datos, S.L.</p>
+                <p>Telf.: 958 958230. E-mail: <a href="mailto:dpd@data-sur.com">dpd@data-sur.com</a>. Chat: <a href="https://www.data-sur.com/">https://www.data-sur.com/</a></p>
+                <p><strong>Finalidad:</strong></p>
+                <p>En oficinadeprofesionales.com tratamos la información que nos facilita según el Reglamento
+                    General de Protección de Datos y Ley Orgánica de Protección de Datos Personales y Garantía
+                    de Derechos Digitales con el fin de gestionar los servicios o productos que le prestamos o nos
+                    demanda mediante la página Web. Todos los datos solicitados (nombre, e-mail, IP, y aquellos
+                    incluidos en la consulta o en los distintos formularios) son estrictamente necesarios para la
+                    gestión de las consultas formuladas, o para publicar su reseña e identificar y localizar el
+                    dispositivo en caso necesario o conseguir cita con un profesional o publicación de anuncios
+                    profesionales, incluyendo la existencia de posibles decisiones automatizadas, no elaborando
+                    perfiles en ningún caso.</p>
+                <p><strong>Almacenamiento:</strong></p>
+                <p>Sus datos personales podrán ser almacenados en los Registros de Actividades de Tratamiento:</p>
+                <p>Formularios Contacto Web: La finalidad de dicho tratamiento es la gestión de las consultas
+                    recibidas a través de los formularios de contacto de la web por parte de los usuarios.</p>
+                <p>Usuarios y/o Publica Tu Anuncio (según proceda): La finalidad de dicho tratamiento es la
+                    gestión y registro como Usuario en nuestra web.</p>
+                <p>Reseñas: La finalidad de dicho tratamiento es la publicación de las reseñas recibidas a través
+                    de los formularios de reseña de la web por parte de los usuarios.</p>
+                <p>Consigue cita: La finalidad de dicho tratamiento es poder facilitarle los datos de contacto al
+                    profesional elegido por el usuario previamente para que se ponga en contacto y poder
+                    responder al servicio o consulta solicitada.</p>
+                <p><strong>Conservación:</strong></p>
+                <p>Los datos proporcionados se conservarán durante el tiempo necesario para la tramitación y
+                    respuesta de la consulta o publicación de la reseña, cumpliendo con la finalidad para la que se
+                    recaban o durante los años necesarios para cumplir con las obligaciones legales.</p>
+                <p>Consigue Cita: Por parte de oficinadeprofesionales.com: Los datos proporcionados se
+                    conservarán durante el tiempo necesario para la tramitación y respuesta de la consulta,
+                    cumpliendo con la finalidad para la que se recaban o durante los años necesarios para cumplir
+                    con las obligaciones legales.</p>
+                <p>Por parte de los profesionales seleccionados el usuario debe de consultar su Política de
+                    Privacidad propia.</p>
+                <p><strong>Comunicación y Transferencia Internacional:</strong></p>
+                <p>Los datos no se cederán a terceros salvo en los casos en que exista una obligación legal y para
+                    la prestación de los servicios ofertados, así haya que hacerlo y no están previstas las
+                    transferencias internacionales de datos.</p>
+                <p>Consigue Cita: Los datos se cederán a los terceros seleccionados por el usuario con la finalidad
+                    de poder ponerse en contacto con el usuario tras solicitarlo mediante un formulario.</p>
+                <p>No están previstas las transferencias internacionales de datos.
+                <p><strong>Legitimación:</strong></p>
+                <p>El tratamiento de sus datos está legitimado mediante el consentimiento expreso del
+                    interesado, que voluntariamente los remite a través del formulario mediante una clara acción
+                    afirmativa.</p>
+                <p><strong>Edad:</strong></p>
+                <p>Los usuarios menores de 14 años necesitan el consentimiento de sus padres, tutores o
+                    representantes legales para la realización de la consulta a través del formulario de contacto. El
+                    usuario reconoce ser mayor de 14 años de edad o disponer del correspondiente
+                    consentimiento de sus padres, tutores o representantes legales para formular la consulta.</p>
+                <p>Los usuarios menores de 18 años no podrán registrase en el Portal.</p>
+                <p><strong>Derechos:</strong></p>
+                <p>Usted tiene derecho a obtener confirmación sobre si el Titular está tratando sus datos
+                    personales, por tanto tiene derecho a acceder a sus datos personales, rectificar los datos
+                    inexactos o solicitar su supresión cuando los datos ya no sean necesarios, así como el derecho
+                    a la limitación u oposición a su tratamiento, portabilidad de sus datos y retirar el
+                    consentimiento aceptado al tratamiento de sus datos e incluso interponer reclamación ante la
+                    Agencia Española de Protección de Datos (<a href="https://www.aepd.es/">https://www.aepd.es/</a>).</p>
+                <p><strong>Comunicaciones Comerciales:</strong></p>
+                <p>En lo referente a comunicaciones comerciales. El Titular se compromete a NO REMITIR
+                    COMUNICACIONES COMERCIALES SIN IDENTIFICARLAS COMO TALES, conforme a lo dispuesto
+                    en la Ley 34/2002 de Servicios de la Sociedad de la Información y de Comercio Electrónico. No
+                    será considerado como comunicación comercial toda la información que se envíe al Usuario
+                    del portal oficinadeprofesionales.com. Siempre que tenga por finalidad el mantenimiento de larelación contractual existente entre usuario y el Titular. Así como el desempeño de las tareas
+                    de información, formación y otras actividades propias del servicio que el cliente/usuario pueda
+                    contratado con la Entidad.</p>
+                <p>El Titular se compromete a través de este medio a NO REALIZAR PUBLICIDAD ENGAÑOSA. A
+                    estos efectos, por lo tanto, no serán considerados como publicidad engañosa los errores
+                    formales o numéricos que puedan encontrarse a lo largo del contenido de las distintas
+                    secciones de la web oficinadeprofesionales.com, producidos como consecuencia de un
+                    mantenimiento y/o actualización incompleta o defectuosa de la información contenida es
+                    estas secciones. El Titular, como consecuencia de lo dispuesto en este apartado, se
+                    compromete a corregirlo tan pronto como tenga conocimiento de dichos errores.</p>
+            </div>      
+        </div>
+    </div>
+</div>
+
+
